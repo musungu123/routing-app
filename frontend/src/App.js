f34be339e4d9841b6d531422edf283ef161b0800
Joyce <joycemusungu2002@gmail.com>
2022-07-01 12:20:51 -0700

    routing app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react'
+
+
+const axios = require('axios');
+useEffect(()=>{
+  axios.get(' ')
+});
+
+export default function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+
