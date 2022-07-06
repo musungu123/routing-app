b554f4e40fa72b3726838a19750f4cdad598f755
Joyce <joycemusungu2002@gmail.com>
2022-07-05 17:35:59 -0700

    upgraded the index.js file

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,39 @@
 const express = require('express')
 require('dotenv').config()
+const cors = require('cors')
 const app = express()
-port = process.env.PORT
+
+app.use(cors())
+
+const users = [
+    {
+      "id": 1,
+       "name": "John", 
+       "email": "john@gmail.com"
+    },
+
+    {"id": 2,
+     "name": "Mary",
+     "email": "mary@gmail.com"
+    },
+
+    {
+      "id": 3,
+     "name": "Peter",
+     "email": "peter@gmail.om"
+    },
+
+]
+
 
 app.get('/', (req, res) => {
-  res.send('hello world')
+    res.send("Public content")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/users', (req, res) => {
+    res.json(users)
 })
+
+const port = process.env.PORT
+
+app.listen(port)
